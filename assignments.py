8288af0b3ebf388cd9d7a04d23fc7879a2dea786
zain raza <zrabbas110@gmail.com>
2024-05-23 02:21:01 +0500

    python assigment first

---
+++ assignments.py
@@ -0,0 +1,75 @@
+# Answer no.1
+
+
+# float_number=float(input("Write the number \t "))
+# print(float_number)
+# rounded_number=round(float_number,2)
+# print(rounded_number)
+# name=input("Write your name \n")
+# namelen=len(name)
+# print(namelen)
+
+
+# Answer no.2
+# sentence=input("Enter your Favort sentences \n")
+# lower_sen=sentence.lower()
+# upper_sen=sentence.upper()
+# capitalize_sen=sentence.capitalize()
+# print(lower_sen)
+# print(upper_sen)
+# print(capitalize_sen)
+
+# Answer 3
+# sentence = input("Please enter a sentence: ")
+
+# word_to_replace = input("Enter the word you would like to replace: ")
+
+# new_word = input("Enter the new word: ")
+
+# modified_sentence = sentence.replace(word_to_replace, new_word)
+
+# print("Modified sentence:", modified_sentence)
+
+
+# Answer 4
+# age=int(input("Write the age \t"))
+# add_age= age + 10
+# print(f"in the 10 years you will be the {add_age}  years old")
+
+# answer 5
+# full_name=input("Enter Full name \n")
+# ful=full_name.upper()
+# print("Your name is "+ful)
+# favort_number=int(input("ENter your favorite number"))
+# multiple=favort_number *  2
+# mutiple_number_str= str(multiple)
+# message= "Your favorite number multiplied by 2 is " + " "+mutiple_number_str+ " "+"."
+# print(message)
+
+# Answer 6
+# first_name=input("Enter first name \n")
+# last_name=input("Enter last name \n")
+
+# full_name= first_name+ " " + last_name
+# full_name_upper=full_name.upper()
+# full_name_replace= full_name_upper.replace(" ","-")
+# calculates_len= len(full_name_replace)
+# print("Your full name in uppercase is:"+full_name_upper)
+# print("Modified sentence:"+full_name_replace)
+# print("The length of your full name is:" +str(calculates_len))
+
+
+# Answer 7
+# number_1= int(input("Enter First Number \n"))
+# number_2= int(input("Enter second Number \n"))
+# average=number_1/number_2
+# average_round= round(average,2)
+# print(average_round)
+
+
+# Answer 8
+# sentence=input("Enter your Sentence :")
+# sentence_with_underscroce= sentence.replace(" ","_")
+# words = sentence.split(" _ ")
+# print("Sentence with under_scroce"+ " "+sentence_with_underscroce)
+# # print("Word in the sentence ",+words)
